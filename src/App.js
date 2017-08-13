5a46a4cf710a5e4bba671d60d0e17a06f3dec00c
Adrian Toman <adrian@adriantoman.com>
2017-08-13 11:22:57 +1000

    Life cycle events

--- src/App.js
+++ src/App.js
@@ -5,45 +5,48 @@ class App extends React.Component {
   constructor() {
     super();
 
-    this.state = {
-      a: '',
-      b: ''}
+    this.state = { val: 0 }
     this.update = this.update.bind(this);
   }
   update(e) {
-    this.setState({
-      a: this.a.refs.input.value,
-      b: this.refs.b.value})
+    this.setState({ val: this.state.val + 1 })
+  }
+  componentWillMount() {
+    console.log('componentWillMount')
+  }
+  componentDidMount() {
+    console.log('componentDidMount')
+  }
+  componentWillUnmount() {
+    console.log('componentWillUnmount')
+  }
+  render() {
+    console.log('render')
+    return (
+      <button onClick={this.update}>
+        {this.state.val}
+      </button>
+    )
+  }
+}
+
+class Wrapper extends React.Component {
+  mount() {
+    ReactDOM.render(<App />, document.getElementById('a'))
+  }
+  unmount() {
+    ReactDOM.unmountComponentAtNode(document.getElementById('a'))
   }
   render() {
     return (
       <div>
-        <Input
-          ref={ component => this.a = component }
-          update={this.update.bind(this)} />
-        {this.state.a}
-        <hr />
-        <input
-          ref="b"
-          type="text"
-          onChange={this.update.bind(this)} />
-        {this.state.b}
+        <button onClick={this.mount.bind(this)}>Mount</button>
+        <button onClick={this.unmount.bind(this)}>Unmount</button>
+        <div id="a"></div>
       </div>
     )
   }
 }
 
-class Input extends React.Component {
-  render() {
-    return (
-      <div>
-        <input
-          ref="input"
-          type="text"
-          onChange={this.props.update} />
-      </div>
-    )
-  }
-}
 
-export default App
+export default Wrapper
